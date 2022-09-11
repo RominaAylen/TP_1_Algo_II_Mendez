ce26f752b5ab063395eb6f3a3833c4fe4e0dbf03
RominaAylen <ragimenez@fi.uba.ar>
2022-09-10 23:43:19 -0300

    refactor main.c

--- main.c
+++ main.c
@@ -24,6 +24,21 @@ struct _caja_t
 	int cantidad_pokemones_guardados;
 };
 
+void info_pokemon(pokemon_t *pokemon)
+{
+	if (!pokemon)
+	{
+		printf("Puntero NULO\n");
+		return;
+	}
+
+	printf("Nombre pokemon: %s\n", pokemon->nombre);
+	printf("Nivel pokemon: %i\n", pokemon->nivel);
+	printf("Ataque pokemon: %i\n", pokemon->poder_ataque);
+	printf("Defensa pokemon: %i\n", pokemon->poder_defensa);
+	printf("-----\n");
+}
+
 int main(int argc, char *argv[])
 {
 	// En este archivo .c tenes que poner tu programa principal.
@@ -36,36 +51,40 @@ int main(int argc, char *argv[])
 	// contenido, combinar un par y mostrar el contenido de la caja
 	// combinada.
 
-	// if (argc != CANTIDAD_FLAGS || strcmp(argv[1], NOMBRE_ARCHIVO_ENTRADA) || strcmp(argv[2], NOMBRE_ARCHIVO_SALIDA))
-	// {
-	// 	printf("Error en los parametros de ejecución. El programa será terminado\n");
-	// 	return ERROR;
-	// }
-
-	caja_t *caja_creada = caja_cargar_archivo(argv[1]);
+	caja_t *caja_creada = caja_cargar_archivo("prueba.csv");
 
 	if (!caja_creada)
 	{
-		printf("NO SE CREO BIEN LA CAJA\n");
+		printf("NO SE CREO BIEN LA CAJA 1\n");
 		return ERROR;
 	}
 
-	pokemon_t pokemon_1 = *(caja_creada->pokemones_guardados[0]);
+	caja_t *caja_creada2 = caja_cargar_archivo("prueba3.csv");
 
-	printf("nombre pokemon 1: %s\n", pokemon_1.nombre);
-	printf("cantidad pokemones: %i\n", caja_creada->cantidad_pokemones_guardados);
-	free(caja_creada);
+	if (!caja_creada2)
+	{
+		caja_destruir(caja_creada);
+		printf("NO SE CREO BIEN LA CAJA 2\n");
+		return ERROR;
+	}
 
-	// t_salida = fopen(NOMBRE_ARCHIVO_SALIDA, "w");
-	// if (!t_salida)
-	// {
-	// 	perror("No se pudo abrir el archivo de escritura");
-	// 	fclose(t_entrada);
-	// 	return ERROR;
-	// }
+	int existosos = caja_recorrer(caja_creada, &info_pokemon);
+	printf("Pokemones Aplicados Exitosos: %i\n", existosos);
 
-	// fclose(t_entrada);
-	// fclose(t_salida);
+	for (int i = 0; i < caja_creada->cantidad_pokemones_guardados; i++)
+	{
+		printf("i: %i\n", i);
+		pokemon_t *pokemon_0 = caja_obtener_pokemon(caja_creada, i);
+		info_pokemon(pokemon_0);
+	}
+	caja_t *caja_combinada = caja_combinar(caja_creada, caja_creada2);
+
+	int escritos = caja_guardar_archivo(caja_combinada, "nuevo.csv");
+	printf("Escritos: %i\n", escritos);
+	caja_destruir(caja_creada);
+	caja_destruir(caja_creada2);
+	caja_destruir(caja_combinada);
+
 	printf("todo joya :P\n");
 
 	return 0;
